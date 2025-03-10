e133cde82709540fd49d935a119daba4cb023807
khuma pokharel <85646390+sujan321-oss@users.noreply.github.com>
2025-03-10 13:11:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express() 
+const PORT = process.env.PORT | 8001
 
 app.get("/",(req,res)=>{
     res.json({"msg":"Hello this message from the server"})
